c901f9f59dfb92e9acadaedab965ab8c025c24f9
Vadim Demedes <vdemedes@gmail.com>
2014-06-04 16:49:23 +0200

    implemented where, returns, size, from, sort

--- test.js
+++ test.js
@@ -2,16 +2,12 @@ var Elasto = require('./');
 
 Elasto.connect('http://localhost:9200/boulevard-development');
 
-Elasto.query('boutiques').near({
-    lat: 42,
-    lon: 13,
-    radius: 3
-}).where({
-   'term1': 'value1',
-   'term2': 'value2' 
-}).where('term3', 'value3')
-  .size(5)
-  .from(5)
-  .returns('first_field', 'second_field')
-  .returns(['first_field', 'second_field'])
-  .search();
+Elasto.query('boutiques')
+        .where('name', 'London')
+        .returns('name', 'address')
+        .sort('name', 'desc')
+        .size(1)
+        .from(1)
+        .search().then(function (documents){
+   console.log(documents); 
+});
