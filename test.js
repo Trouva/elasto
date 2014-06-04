51858bac1469b57e842c34c7aa46b710ccbd5e6f
Vadim Demedes <vdemedes@gmail.com>
2014-06-04 15:28:01 +0200

    removed Backbone

--- test.js
+++ test.js
@@ -2,10 +2,6 @@ var Elasto = require('./');
 
 Elasto.connect('http://localhost:9200/boulevard-development');
 
-var Boutique = Elasto.Document.extend({
-   name: 'boutiques' 
-});
-
-Boutique.find().then(function (boutiques) {
+Elasto.query('boutiques').find().then(function (boutiques) {
    console.log(boutiques); 
 });
