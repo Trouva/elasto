0b30e2195ac31287da64e3056a1a9e755d7b9f52
Vadim Demedes <vdemedes@gmail.com>
2014-06-04 18:21:55 +0200

    find() method

--- test.js
+++ test.js
@@ -3,12 +3,8 @@ var Elasto = require('./');
 Elasto.basePath = 'http://localhost:9200/boulevard-development';
 
 Elasto.query('boutiques')
-        .near({
-            lat: 51.5,
-            lon: -0.1467912,
-            radius: 3
-        })
-        .returns('name', 'address')
-        .search().then(function (documents){
+        .where('slug', 'cosi-homewares-in-n103hp')
+        .returns('slug', 'name', 'address')
+        .find().then(function (documents){
    console.log(documents); 
 });
