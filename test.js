93d0d2461e541a69d017335364625d4a843c82ab
Vadim Demedes <vdemedes@gmail.com>
2014-06-04 16:23:22 +0200

    added query methods

--- test.js
+++ test.js
@@ -2,6 +2,16 @@ var Elasto = require('./');
 
 Elasto.connect('http://localhost:9200/boulevard-development');
 
-Elasto.query('boutiques').find().then(function (boutiques) {
-   console.log(boutiques); 
-});
+Elasto.query('boutiques').near({
+    lat: 42,
+    lon: 13,
+    radius: 3
+}).where({
+   'term1': 'value1',
+   'term2': 'value2' 
+}).where('term3', 'value3')
+  .size(5)
+  .from(5)
+  .returns('first_field', 'second_field')
+  .returns(['first_field', 'second_field'])
+  .search();
