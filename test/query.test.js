c932f417146b0524676b2c2e20479d0079a1a269
Arnaud Benard <arnaud@streethub.com>
2014-06-05 11:05:01 +0100

    testing to find items with multiple params

--- test/query.test.js
+++ test/query.test.js
@@ -5,36 +5,42 @@ var Elasto = require('../');
 
 Elasto.basePath = 'http://localhost:9200/boulevard-development';
 
+var product = {
+    slug: 'fig-tree-reed-diffuser-from-orla-kiely',
+    boutique_slug: 'cosi-homewares-in-n103hp'
+};
+
 describe('Elasto', function() {
   describe('find', function() {
 
     it('should find a specific item', function(done) {
+        var slug = product.boutique_slug;
 
         Elasto.query('boutiques')
-        .where('slug', 'cosi-homewares-in-n103hp')
+        .where('slug', slug)
         .find().then(function (documents){
 
             assert.equal(documents.length, 1);
             documents.should.not.be.equal(undefined);
+            documents[0].slug.should.be.equal(slug);
 
-            console.log('putain');
-            console.log(documents);
             done();
         });
 
     });
 
     it('should find a specific item with multiple params', function(done) {
+        var boutique_slug = product.boutique_slug;
+        var slug = product.slug;
 
         Elasto.query('products')
-        .where({
-            slug: 'cosi-homewares-in-n103hp',
-            boutique_slug: 'fig-tree-reed-diffuser-from-orla-kiely'
-        })
+        .where(product)
         .find().then(function (documents){
 
             documents.length.should.be.equal(1);
             documents.should.not.be.equal(undefined);
+            documents[0].slug.should.be.equal(slug);
+            documents[0].boutique_slug.should.be.equal(boutique_slug);
 
             done();
         });
